95a1faf6269d1c7206a9c96011410c070315e2af
bozoten <shridharapavel@gmail.com>
2025-01-31 17:02:48 -0500

    Contains-Duplicate-III

---
+++ contains-duplicate-III.py
@@ -0,0 +1,31 @@
+'''
+I wouldn't say this si the most optimal solution, technicalyl it works but the time complexity could be much better. i still 
+don't understand the solution for this problem, so ima just leave this here and come back to it later.
+'''
+
+class Solution(object):
+    def containsNearbyAlmostDuplicate(self, nums, indexDiff, valueDiff):
+        """
+        :type nums: List[int]
+        :type indexDiff: int
+        :type valueDiff: int
+        :rtype: bool
+        """
+        
+        hashmap = defaultdict(int)
+        l = 0
+
+        for r in range(len(nums)):
+            if abs(l - r) > indexDiff:
+                l += 1
+                hashmap[nums[l]] -= 1
+
+            hashmap[nums[r]] += 1
+
+            while i < j:
+                if abs(nums[i] - nums[j]) <= valueDiff and i!=j:
+                    return True
+                
+                i += 1
+    
+        return False
