10fe740ddd67bd29b23dd245322ddc2c4bed8dd9
viletbird <shridharapavel@gmail.com>
2025-04-20 07:44:13 -0400

    bstkthsmallest optimal solution

--- bstkthsmallest.py
+++ bstkthsmallest.py
@@ -28,3 +28,52 @@ class Solution:
 
         return array[k]
         
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+class Solution:
+    def kthSmallest(self, root: Optional[TreeNode], k: int) -> int:
+        array = []
+
+        def dfs(node):
+            if node is None:
+                return 0
+
+            dfs(node.left)
+            array.append(node)
+            dfs(node.right)
+
+        dfs(root)
+        return array[k - 1].val
+                    
+
+                
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+class Solution:
+    def kthSmallest(self, root: Optional[TreeNode], k: int) -> int:
+        array = []
+
+        def dfs(node):
+            if node is None:
+                return 0
+
+            dfs(node.left)
+            array.append(node)
+            dfs(node.right)
+
+        dfs(root)
+        return array[k - 1].val
+                    
+
+                
+        
