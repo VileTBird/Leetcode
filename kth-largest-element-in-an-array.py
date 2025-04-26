87ab019dd4495d7a2e4fd6cc476e81b66dd17050
viletbird <shridharapavel@gmail.com>
2025-04-26 07:52:33 -0400

    removing redundancy

--- kth-largest-element-in-an-array.py
+++ kth-largest-element-in-an-array.py
@@ -11,4 +11,13 @@ class Solution:
         
         return res[0] if res else 0
         
+class Solution:
+    def findKthLargest(self, nums: List[int], k: int) -> int:
+        heapq.heapify(nums)
+
+        while len(nums) > k:
+            heapq.heappop(nums)
+        
+        return nums[0] if nums else 0
+        
         
